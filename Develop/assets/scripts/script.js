e1444013eb0eb23e2988a20a45fc146088bad605
LayneHansen <layne.d.hansen@gmail.com>
2020-08-06 22:06:49 -0600

    function of generatePassword with external variables

--- Develop/assets/scripts/script.js
+++ Develop/assets/scripts/script.js
@@ -7,12 +7,13 @@
   
   // for (characters i=8; i <= 128; i++);
 
-  let charAmount = document.querySelector("#characters")
+ // let charAmount = document.querySelector("#charAmount")
+
+
+  // 2. CHARACTERS (numbers, uppercase letters, lowercase letters, symbols)
+
+  /* if user checks box, include numbers
   
-
- // 2. CHARACTERS (numbers, uppercase letters, lowercase letters, symbols)
-
-  // if user checks box, include numbers
   var numbers = document.querySelector("#numbers");
   console.log(numbers);
 
@@ -30,6 +31,25 @@
 
   var symbols = document.querySelector("#symbols")
   console.log(symbols);
+
+  */
+
+  
+  /*
+
+  EVENT LISTENERS FOR BOX CHECKS
+
+  numbers.addEventListener('checkbox', charTypes);
+  lowCase.addEventListener('checkbox', charTypes);
+  uppCase.addEventListener('checkbox', charTypes);
+  symbols.addEventListener('checkbox', charTypes);
+
+  */
+
+  // CREATE charTypes variable
+
+  // charTypes () {}
+
 
   // VARIABLE to combine these selections
   
@@ -61,26 +81,48 @@ var symbols = confirm("do you want special symbols?")
 */
 
 
-
-// generate symbol
-
-function getRandomSymbol () {
-  const symbols = '!@#$%&'
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-console.log(getRandomSymbol());
-
-
 // GENERATE BUTTON
 
 var generateBtn = document.querySelector("#generate");
 console.log(generateBtn);
 
-// GENERATE PASSWORD FUNCTION
+/* GENERATE PASSWORD FUNCTION
+  How many characters do they want 8-128?
+  if/else statement for how many
+  alert, "You didn't select a correct amount"
+  return statement, cancel execution
+  
+  Which character types do you want
+  you must select at least one
+  if selected, concatonate
+  match number of characters to user input
+  randomize type and order
+  create a variable that shows what they want to return
+
+*/
+
+// const charAmount = document.querySelector("#charAmount");
+// charAmount.addEventListener('keydown', "");
 
 function generatePassword () {
-  // set password length
+  
+  
+  var results = "";
+  var charAmountInput = charAmount.value;
+  var numberInput = num;
+  var lowerCaseInput = lower;
+  var upperCaseInput = upper;
+  var symbolInput = symbol;
+  
+  for (var i=1; i <= 8 ;i++) {
+    results = results + numberInput + lower + upper + symbol;
+  } 
+  return results;
+
 }
+
+generatePassword ();
+console.log(generatePassword());
 
 // Write password to the #password input
 
@@ -95,27 +137,42 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-
 // GENERATE CHARACTERS
 
 //generate lower case letter
 
-function getRandomLower () {
+function randomLower () {
   return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
 }
-console.log(getRandomLower());
+console.log(randomLower());
 
 //generate upper case letter
 
-function getRandomUpper () {
+function randomUpper () {
   return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
 }
-console.log(getRandomUpper());
+console.log(randomUpper());
 
 // generate number
 
 var num = Math.floor(Math.random() * 10);
 console.log(num);
+
+// generate symbol
+
+function randomSymbol () {
+  const symbols = '!@#$%&'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+console.log(randomSymbol());
+
+// VARIABLES THAT PULL CHARACTERS
+
+
+
+var lower = randomLower ();
+var upper = randomUpper ();
+var symbol = randomSymbol ();
 
 
 
